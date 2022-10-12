f957efa8a8c85a16de8b7ec36f82b55555bcd8da
Casper <Casper@10.10.201.71>
2022-10-12 22:09:32 +0300

    Dairenin Alanı

---
+++ DaireAlanıCevre/src/Main/Main.java
@@ -0,0 +1,19 @@
+package Main;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double r,pi;
+		r=2.8;
+		pi=3.14;
+		double cevre=2*pi*r;
+		double alan=pi*r*r;
+		
+		System.out.println("Dairenin Çevresi: "+cevre+" Dairenin Alanı: "+alan);
+		
+		
+		
+
+	}
+
+}
